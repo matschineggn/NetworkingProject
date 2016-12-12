03d150fdfd946ac8f7b56042a723192850d57b99
MatschineggN <matschineggn@msoe.edu>
2016-12-12 00:42:18 -0600

    changes

--- Networking_Project/Test_File.c
+++ Networking_Project/Test_File.c
@@ -11,10 +11,36 @@
 #include <MSOE/bit.c>
 #include <avr/interrupt.h>
 
+void setup(void);
+
+int state;
+int idle = 1;
+int busy = 2;
+int collision = 3;
 
 int main(void)
 {
+	setup();
 
+	switch(state)
+		{
+			case 1:		// Idle State
 
+			break;
 
+			case 2:		// Busy State
+
+			break;
+
+			case 3:		// Collision State
+
+			break;
+		}
+
+}
+
+void setup(void)
+{
+	DDRD 	|= (1<<3);	// PD3(output), PD2(input)
+	PORTD 	|= (1<<3);	// PD3(high), PD2(low)
 }
