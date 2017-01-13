d5e581a0e323a64fb5013c8941462dfe36dd4411
matschineggn <matschineggn@msoe.edu>
2017-01-13 14:56:09 -0600

    Adding Tranmitter code. Left off in Tranmist( ) function.

--- Networking_Project/Test_File.c
+++ Networking_Project/Test_File.c
@@ -9,14 +9,41 @@
 //#include <MSOE_I2C/delay.h>
 #include <MSOE/bit.c>
 #include <avr/interrupt.h>
+#include <stdbool.h>
 
 #include "states.h"
 #include "interrupts.h"
 #include "timer.h"
 #include "FSM.h"
 
+#define FOSC 1843200 // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
 void pinSetup(void);
 //void stateCheck(void);
+void transmitter(void);
+void Transmit(int);
+void USART_Init( unsigned int );
+unsigned char USART_Receive(void);
+void manchesterOne(void);
+void manchesterZero(void);
+
+bool stringComplete = false;  // whether the string is complete
+// reserve 200 bytes for the inputString:
+char inputString[80];
+char inChar;
+float charCon;
+int charVect[8];
+int msbVect[8];
+int lsbVect[8];
+int character;
+int charIN;
+int intChar;
+int transmit;
+int maxDecVal;
+int n;
+int N;
 
 int Tb = 215; 	// period in micro second (400us = 0.4ms) (Using 215 to obtain proper threshold)
 // I think that Tb needs to be around half the 400us because it is acting like the sample frequency
@@ -29,8 +56,12 @@ int Tb = 215; 	// period in micro second (400us = 0.4ms) (Using 215 to obtain pr
 //int whileEscape;			// declared in "interrupts.h"
 //int count;				// declared in "timer.h"
 
+
+
 int main(void)
 {
+	USART_Init(MYUBRR);
+
 	pinSetup();
 	interruptSetup();
 
@@ -64,13 +95,166 @@ void pinSetup(void)
 }
 
 
+void transmitter(void)
+{
+// 2500 bps = 0.4 ms = 400 us
+// 5000 bps = 0.2 ms = 200 us
+
+	stringComplete = false;
+
+//	/* Wait for data to be received */
+//	while ( !(UCSR0A & (1<<RXC0)) )	;
+
+	if (UCSR0A && (1<<RXC0))	//if new data, get it
+	{
+//		N = 0;
+
+		while(stringComplete == false)
+		{
+			//get the new byte:
+			 inChar = USART_Receive();
+			//add it to the inputString:
+			inputString[N] = inChar;
+
+			N = N+1;			// counts number of characters
+
+			if (inChar == '\r')
+			{
+				stringComplete = true;
+				transmit = 1;
+				n = 0;
+			}
+		}
+	}
+
+	while(transmit == 1)
+	{
+//		for (n=0 ; n==N ; n+1)
+
+		if( inputString[n] != '\0' )
+		{
+			character = inputString[n];
+			charIN = character;
+
+			Transmit(charIN);
+
+		}
+
+	}
+
+}
+
+void Transmit(int charIN)
+{
+	charCon = charIN;
+
+	for(n=0; n<5; n++)
+	{
+		maxDecVal = (2^(8-n));
+		if(charCon >= maxDecVal)
+		{
+			charVect[n] = 1;
+		}
+		else
+		{
+			charVect[n] = 0;
+		}
+	}
+
+	// Convert characters to 8 bit strings
+	if(charCon >= 128)
+	{
+		charVect[n] = '1';
+		n = n+1;
+	}
+	else
+		{
+			n = n+1;
+		}
+
+	if(charCon >= 128)
+		{
+			charVect[n] = '1';
+			n = n+1;
+		}
+		else
+			{
+				n = n+1;
+			}
 
 
+//	switch(charIN)
+//	{
+//
+//	case 97 :		// a
+//
+//		break;
+//
+//	case 98 :		// b
+//
+//		break;
+//
+//	case 99 :		// c
+//
+//	break;
+//
+//	case  :			// d
+//
+//		break;
+//
+//	case  :			// e
+//
+//		break;
+//
+//
+//	default:		// f
+//
+//		break;
+//	}
+
+}
 
 
+void USART_Init( unsigned int ubrr)
+{
+// Set Rx as input
 
+/*Set baud rate */
+UBRR0H = (unsigned char)(ubrr>>8);
+UBRR0L = (unsigned char)ubrr;
+//Enable receiver and transmitter
+UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+/* Set frame format: 8data, 2stop bit */
+UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
 
+unsigned char USART_Receive(void)
+{
+///* Wait for data to be received */
+//while ( !(UCSR0A & (1<<RXC0)) )	;
+/* Get and return received data from buffer */
+return UDR0;
+}
 
+void manchesterOne(void)
+{
+	//Tx High
+
+	delay_us(200);
+	//Tx Low
+
+	delay_us(200);
+}
+
+void manchesterZero(void)
+{
+	//Tx Low
+
+	delay_us(200);
+	//Tx High
+
+	delay_us(200);
+}
 
 
 
